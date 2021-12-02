fa01b3decd6230a91cc25f5959e704e648c59396
Kevin <kevin.heater@gmail.com>
2021-12-01 21:03:33 -0700

    Day One

---
+++ 2021/01/CSharp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DayOneCSharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = TestDataHelper.GetData().ToArray();
+
+            var answer = data
+                .Skip(1)
+                .Zip(data)
+                .Select(pair => pair.First > pair.Second ? 0 : 1)
+                .Sum();
+
+            Console.WriteLine(answer);
+        }
+
+
+    }
+    public static class TestDataHelper 
+    {
+        public static IEnumerable<int> GetData()
+        {
+            var lines = File.ReadAllText(@".\Data.txt").Split(Environment.NewLine);
+
+            foreach (var line in lines)
+            {
+                if (Int32.TryParse(line, out var result))
+                {
+                    yield return result;
+                }
+            }
+        }
+    }
+
+    public static class IEnumerableExtensionMethods
+    {
+        public static IEnumerable<(T a, T b)> Pairs<T>(this IEnumerable<T> source)
+        {
+            var firstOrNull = source.FirstOrDefault();
+
+            if (firstOrNull == null)
+                yield break;
+
+            T previous = firstOrNull ?? default;
+
+            foreach (var item in source.Skip(1))
+            {
+                yield return (previous, item);
+                previous = item;
+            }
+        }
+
+    }
+}
---
+++ 2021/01/FSharp/Program.fs
@@ -0,0 +1,23 @@
+﻿open System.IO;
+open System;
+
+// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+// See the 'F# Tutorial' project for more help.
+
+// Define a function to construct a message to print
+let readData path : string[] = File.ReadAllLines(path);
+
+let converToInt (value : string) : int = Convert.ToInt32(value)
+
+let oneIfGreater (pair : (int * int)) : int = match pair with a,b when a > b -> 1 | _ -> 0
+
+[<EntryPoint>]
+let main argv =
+    let data = readData(@".\Data.txt") |> Array.map converToInt |> Array.toSeq
+    let nextValue = data |> Seq.skip 1
+    let result = data |> Seq.zip nextValue |> Seq.map oneIfGreater |> Seq.sum
+
+    printf $"{result}"
+    0 // return an integer exit code
+
+   
